4da311174712b0bf995a6d09c8e513a3c26f4b81
Cathy Hu <cathy.hu@fau.de>
2020-07-01 20:36:53 +0200

    [misc] Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='kibicara',
+    version='0.1.0',
+    description='distribute messages across different social media',
+    url='https://github.com/acipm/kibicara',
+    license='0BSD',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'kibicara=kibicara.kibicara:Main',
+        ]
+    },
+    install_requires=[
+        'aiofiles',
+        'aiosqlite',
+        'argon2_cffi',
+        'fastapi',
+        'hypercorn',
+        'ormantic @ https://github.com/dl6tom/ormantic/tarball/bugfix__get_key_factory#egg=ormantic-0.0.32',
+        'passlib',
+        'peony-twitter[all]',
+        'pynacl',
+        'python-multipart',
+        'pytoml',
+        'scrypt',
+    ],
+    test_requires=[
+        'pytest',
+    ],
+)
