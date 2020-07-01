c05d8cadadf68c47aef7c277867513d793502641
Cathy Hu <cathy.hu@fau.de>
2020-07-01 21:21:39 +0200

    [core] Add parser for configuration

---
+++ kibicara/config.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2020 by Thomas Lindner <tom@dl6tom.de>
+# Copyright (C) 2020 by Cathy Hu <cathy.hu@fau.de>
+#
+# SPDX-License-Identifier: 0BSD
+
+from optparse import OptionParser
+from pytoml import load
+
+
+config = {
+        'database_connection': 'sqlite:////tmp/kibicara.sqlite',
+        'frontend_path': None,
+    }
+
+parser = OptionParser()
+parser.add_option('-f', dest='configfile', default='/etc/kibicara.conf')
+(option, args) = parser.parse_args()
+
+try:
+    with open(option.configfile) as configfile:
+        config.update(load(configfile))
+except FileNotFoundError:
+    # run with default config
+    pass
