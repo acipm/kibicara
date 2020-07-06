cd62b5b0a7f8558009838956e7597035f5f21e95
Cathy Hu <cathy.hu@fau.de>
2020-07-06 11:14:21 +0200

    [twitter] Connect Twitter bot to core

--- kibicara/webapi/__init__.py
+++ kibicara/webapi/__init__.py
@@ -5,6 +5,8 @@
 
 from fastapi import APIRouter
 from kibicara.platforms.test.webapi import router as test_router
+from kibicara.platforms.twitter.webapi import router as twitter_router
+from kibicara.platforms.twitter.webapi import twitter_callback_router
 from kibicara.webapi.admin import router as admin_router
 from kibicara.webapi.hoods import router as hoods_router
 from kibicara.webapi.hoods.badwords import router as badwords_router
@@ -16,4 +18,8 @@ router.include_router(admin_router, prefix='/admin', tags=['admin'])
 hoods_router.include_router(triggers_router, prefix='/{hood_id}/triggers')
 hoods_router.include_router(badwords_router, prefix='/{hood_id}/badwords')
 hoods_router.include_router(test_router, prefix='/{hood_id}/test', tags=['test'])
+hoods_router.include_router(
+    twitter_router, prefix='/{hood_id}/twitter', tags=['twitter']
+)
+router.include_router(twitter_callback_router, prefix='/twitter', tags=['twitter'])
 router.include_router(hoods_router, prefix='/hoods', tags=['hoods'])
