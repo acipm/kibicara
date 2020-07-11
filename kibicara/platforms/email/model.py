ec053381826fdbb2c7983e4acd15b6ed33f6ab74
maike <maike@systemli.org>
2020-07-11 04:26:08 +0200

    [email] Column is supposed to be unique

--- kibicara/platforms/email/model.py
+++ kibicara/platforms/email/model.py
@@ -21,7 +21,7 @@ class Email(Model):
     """ This table is used to track the hood ID. It also stores the token secret. """
 
     id: Integer(primary_key=True) = None
-    hood: ForeignKey(Hood)
+    hood: ForeignKey(Hood)  # this is supposed to be unique - dl6tom/ormantic/pulls/1
     secret: Text()
 
     class Mapping(Mapping):
