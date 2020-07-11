995b6e34d9fb51cd0fbd0edea8f1a2d6b55e5489
maike <maike@systemli.org>
2020-07-11 04:29:44 +0200

    [tests] Unauthorized: delete email bot, write message

--- tests/test_api_email_unauthorized.py
+++ tests/test_api_email_unauthorized.py
@@ -10,6 +10,17 @@ def test_email_create_unauthorized(client, hood_id):
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-def test_email_delete_unauthorized(client, hood_id):
-    response = client.delete('/api/hoods/%d/email/' % hood_id)
+def test_email_delete_unauthorized(client, hood_id, email_row):
+    response = client.delete('/api/hoods/%d/email/%d' % (hood_id, email_row['id']))
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+# def test_email_delete_of_different_hood
+
+
+def test_email_message_unauthorized(client, hood_id, email_row):
+    body = {"text": "test", "author": "author", "secret": "wrong"}
+    response = client.post(
+        '/api/hoods/%d/email/messages/%d' % (hood_id, email_row['id']), json=body
+    )
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
