e308d0d83b61c8667785595b6f33db61326da0ca
Cathy Hu <cathy.hu@fau.de>
2020-07-25 22:20:35 +0200

    [frontend] Add homepage and not found routes

--- kibicara-frontend/src/app/app-routing.module.ts
+++ kibicara-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
