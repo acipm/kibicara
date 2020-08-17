c77d5fd25eb151a5f2d499ab0974ec1369d5301d
Cathy Hu <cathy.hu@fau.de>
2020-08-17 16:09:07 +0200

    [frontend] Add minimal example for fetching hood list

--- kibicara-frontend/src/app/hoodspage/hoodspage.component.ts
+++ kibicara-frontend/src/app/hoodspage/hoodspage.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { HoodsService } from '../api/api/hoods.service';
 
 @Component({
   selector: 'app-hoodspage',
   templateUrl: './hoodspage.component.html',
-  styleUrls: ['./hoodspage.component.scss']
+  styleUrls: ['./hoodspage.component.scss'],
 })
 export class HoodspageComponent implements OnInit {
+  hoods$ = this.hoodsService.getHoods();
 
-  constructor() { }
+  constructor(private readonly hoodsService: HoodsService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
