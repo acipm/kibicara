0ec30a4a44d6f327cbbd6918026ae82e45cc1067
Cathy Hu <cathy.hu@fau.de>
2020-08-27 17:04:22 +0200

    [frontend] Finish navigation bar

--- kibicara-frontend/src/app/shared/toolbar/toolbar.component.ts
+++ kibicara-frontend/src/app/shared/toolbar/toolbar.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/core/auth/login.service';
+import { Observer } from 'rxjs';
+import { Route } from '@angular/compiler/src/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { first, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
+  authenticated = false;
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router) {
+    this.router.routeReuseStrategy.shouldReuseRoute = function () {
+      return false;
+    };
 
-  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.router.navigated = false;
+        if (this.loginService.currentHoodAdminValue) {
+          this.authenticated = true;
+        } else {
+          this.authenticated = false;
+        }
+      }
+    });
   }
 
+  ngOnInit(): void {}
+
+  onLogout() {
+    this.loginService.logout();
+    this.router.navigate(['/']);
+  }
 }
