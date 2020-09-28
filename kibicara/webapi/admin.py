88ab5e6ce8542d97c7886de153f185faf0aaba0d
Martin Rey <martin.rey@mailbox.org>
2020-09-29 00:54:34 +0200

    [misc] Add member to copyright section of edited files

--- kibicara/webapi/admin.py
+++ kibicara/webapi/admin.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2020 by Thomas Lindner <tom@dl6tom.de>
 # Copyright (C) 2020 by Cathy Hu <cathy.hu@fau.de>
 # Copyright (C) 2020 by Christian Hagenest <c.hagenest@pm.me>
+# Copyright (C) 2020 by Martin Rey <martin.rey@mailbox.org>
 #
 # SPDX-License-Identifier: 0BSD
 
